347efd8ee12238ab478892f332ac1cf42e233798
Izabel Nascimento <izabelyale@hotmail.com>
2023-03-25 01:54:30 -0300

    sorting algorithms

---
+++ main.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "ordenacao.c"
+#define N 10000
+
+// Izabel Nascimento
+void lerArquivo(int *numsBubble, int *numsSelection, int *numsInsertion, int *numsMerge, int *numsQuick);
+void atualizarArquivo(int algt, int *vet);
+
+int main()
+{
+    time_t time;
+    int *numsBubble, *numsSelection, *numsInsertion, *numsMerge, *numsQuick;
+
+    numsBubble = (int *)malloc(N * sizeof(int));
+    numsSelection = (int *)malloc(N * sizeof(int));
+    numsInsertion = (int *)malloc(N * sizeof(int));
+    numsQuick = (int *)malloc(N * sizeof(int));
+    numsMerge = (int *)malloc(N * sizeof(int));
+    lerArquivo(numsBubble, numsSelection, numsInsertion, numsMerge, numsQuick);
+
+    time = clock();
+    bubbleSort(numsBubble, N);
+    time = clock() - time;
+    printf("- BUBBLE SORT -\n");
+    printf("Tempo de execucao: %.2lf milissegundos\n", ((double)time));
+    atualizarArquivo(1, numsBubble);
+
+    time = clock();
+    selectionSort(numsSelection, N);
+    time = clock() - time;
+    printf("- SELECTION SORT -\n");
+    printf("Tempo de execucao: %.2lf milissegundos\n", ((double)time));
+    atualizarArquivo(2, numsSelection);
+
+    time = clock();
+    insertionSort(numsInsertion, N);
+    time = clock() - time;
+    printf("- INSERTION SORT -\n");
+    printf("Tempo de execucao: %.2lf milissegundos\n", ((double)time));
+    atualizarArquivo(3, numsInsertion);
+
+    time = clock();
+    mergeSort(numsMerge, 0, N);
+    time = clock() - time;
+    printf("- MERGE SORT -\n");
+    printf("Tempo de execucao: %.2lf milissegundos\n", ((double)time));
+    atualizarArquivo(4, numsMerge);
+
+    /*time = clock();
+    quickSort(numsQuick, N);
+    time = clock() - time;
+    printf("- QUICK SORT -\n");
+    printf("Tempo de execucao: %.2lf milissegundos\n", ((double)time));
+    atualizarArquivo(5, numsQuick);;*/
+
+    return 0;
+}
+
+void lerArquivo(int *numsBubble, int *numsSelection, int *numsInsertion, int *numsMerge, int *numsQuick)
+{
+    FILE *arq;
+    arq = fopen("numeros.txt", "r");
+
+    if (arq == NULL)
+    {
+        printf("Problemas na abertura do arquivo\n");
+        getchar();
+        exit(0);
+    }
+
+    int i = 0;
+    char w[100], *y;
+
+    while (fgets(w, 100, arq) != NULL)
+    {
+        y = malloc(100 * sizeof(int));
+        int tam = strlen(w);
+        for (int j = 0; j < tam - 1; j++)
+        {
+            y[j] = w[j];
+        }
+        numsBubble[i] = atoi(y);
+        numsSelection[i] = atoi(y);
+        numsInsertion[i] = atoi(y);
+        numsMerge[i] = atoi(y);
+        numsQuick[i] = atoi(y);
+        free(y);
+        i++;
+    }
+    fclose(arq);
+}
+
+void atualizarArquivo(int algt, int *vet)
+{
+    FILE *arq;
+    switch (algt)
+    {
+    case 1:
+        arq = fopen("bubbleSort.txt", "w");
+        break;
+    case 2:
+        arq = fopen("selectionSort.txt", "w");
+        break;
+    case 3:
+        arq = fopen("insertionSort.txt", "w");
+        break;
+    case 4:
+        arq = fopen("mergeSort.txt", "w");
+        break;
+    case 5:
+        arq = fopen("quickSort.txt", "w");
+        break;
+    }
+
+    if (arq == NULL)
+    {
+        printf("Problemas na abertura do arquivo aq\n");
+        getchar();
+        exit(0);
+    }
+
+    char s[200];
+    int result;
+    for (int i = 0; i < N; i++)
+    {
+        sprintf(s, "%d", vet[i]);
+        result = fputs(s, arq);
+        if (result == EOF)
+        {
+            printf("Erro na gravacao!\n");
+        }
+        result = fputs("\n", arq);
+    }
+}
---
+++ ordenacao.c
@@ -0,0 +1,110 @@
+
+void bubbleSort(int *vet, int n)
+{
+    int i, j, aux;
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 0; j < i; j++)
+        {
+            if (vet[j] > vet[j + 1])
+            {
+                aux = vet[j];
+                vet[j] = vet[j + 1];
+                vet[j + 1] = aux;
+            }
+        }
+    }
+}
+
+void selectionSort(int *vet, int n)
+{
+    int min, i, j, aux;
+    for (i = 0; i <= n - 2; i++)
+    {
+        min = i;
+        for (j = i + 1; j <= n - 1; j++)
+        {
+            if (vet[j] < vet[min])
+            {
+                min = j;
+            }
+        }
+        aux = vet[i];
+        vet[i] = vet[min];
+        vet[min] = aux;
+    }
+}
+
+void insertionSort(int *vet, int n)
+{
+    int key, i, j;
+    for (j = 1; j < n; j++)
+    {
+        key = vet[j];
+        i = j - 1;
+        while (i >= 0 && vet[i] > key)
+        {
+            vet[i + 1] = vet[i];
+            i = i - 1;
+        }
+        vet[i + 1] = key;
+    }
+}
+
+void merge(int *vet, int inicio, int meio, int fim)
+{
+    int a = meio - inicio + 1;
+    int b = fim - meio;
+    int *left, *right, i, j, k;
+
+    left = malloc(a * sizeof(int));
+    right = malloc(b * sizeof(int));
+
+    for (i = 0; i < a; i++)
+    {
+        left[i] = vet[inicio + i];
+    }
+    for (j = 0; j < b; j++)
+    {
+        right[j] = vet[meio + j + 1];
+    }
+    i = 0;
+    j = 0;
+    k = inicio;
+    while (i < a && j < b) {
+        if (left[i] <= right[j]) {
+            vet[k] = left[i];
+            i++;
+        }
+        else {
+            vet[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < a) {
+        vet[k] = left[i];
+        i++;
+        k++;
+    }
+    while (j < b) {
+        vet[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int *vet, int inicio, int fim)
+{
+    if (inicio < fim)
+    {
+        int meio = inicio + (fim - inicio) / 2;
+        mergeSort(vet, inicio, meio);
+        mergeSort(vet, meio + 1, fim);
+        merge(vet, inicio, meio, fim);
+    }
+}
+
+/*void quickSort(int *vet, int n){
+
+}*/
